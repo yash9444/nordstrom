02b19ca5c1def3e8b676a35c5383f242961f9814
Soumyakant Swain <soumyakantswain5@gmail.com>
2022-05-05 00:43:18 +0530

    signup and signin page updated

---
+++ signup/signin.js
@@ -0,0 +1,27 @@
+document.querySelector("#next").addEventListener("click", nextFunction)
+
+function nextFunction () {
+    var Email = document.querySelector("#email").value;
+    
+    if(Email.length !==  0){
+        var arr = JSON.parse(localStorage.getItem("userDataList"))
+        if(arr !== null) {
+            for(var i = 0; i < arr.length; i++) {
+                if(arr[i].userEmail == Email) {
+                    window.location.href = "signin2.html"
+                }
+                else {
+                    localStorage.setItem("userEmail", JSON.stringify(Email))
+                    window.location.href = "signup.html";
+                }
+            }
+        }
+        else {
+            localStorage.setItem("userEmail", JSON.stringify(Email))
+            window.location.href = "signup.html";
+        }
+    }
+    else {
+        alert("Enter Your Email")
+    }
+}
---
+++ signup/signin2.js
@@ -0,0 +1,23 @@
+var Email = JSON.parse(localStorage.getItem("userEmail"));
+document.querySelector("#emailPara").innerText = Email;
+
+document.querySelector("#signinBtn").addEventListener("click", loginFunction);
+    
+function loginFunction() {
+    var password = document.querySelector("#inputPassword").value;
+    var Email = JSON.parse(localStorage.getItem("userEmail"));
+    
+    var arr = JSON.parse(localStorage.getItem("userDataList"))
+    var found = true;
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i].userEmail == Email && Number(arr[i].userPassword) == Number(password)) {
+            alert("Login successful")
+        }
+        else {
+            found = false
+        }
+    }
+    if (found == false) {
+        alert("Please enter valid credentials")
+    }
+}
